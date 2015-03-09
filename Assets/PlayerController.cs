d663c63423782beb8b381ca29fdd03de39426041
Alice Fredine <alice.fredine@gmail.com>
2015-03-08 21:16:41 -0700

    temp good and bad sound triggers

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,9 +5,14 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Camera camera;
-
+	public AudioSource goodSound;
+	public AudioSource badSound;
 	// Use this for initialization
 	void Start () {
+
+		AudioSource[] audios = GetComponents<AudioSource>();
+		goodSound = audios [0];
+		badSound = audios [1];
 	
 	
 	}
@@ -17,4 +22,13 @@ public class PlayerController : MonoBehaviour {
 
 		transform.Translate(camera.transform.forward * speed * Time.deltaTime);
 	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Good") {
+			goodSound.Play ();
+	}
+		if(other.gameObject.tag == "Bad") {
+			badSound.Play ();
+		}
+}
 }
