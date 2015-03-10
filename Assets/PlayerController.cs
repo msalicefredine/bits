02c9dd8d48cac06b864092080b11a9847e00a5dc
Andrew Bates <andrewbatesubc@gmail.com>
2015-03-09 17:44:17 -0700

    prefabbed stuff, everything works

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
-using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
-	public float speed;
-	public Camera camera;
-
-	// Use this for initialization
-	void Start () {
-	
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		//transform.Translate(camera.transform.forward * speed * Time.deltaTime);
-		transform.position = transform.position + camera.transform.forward * speed * Time.deltaTime;
-	}
-}
-=======
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -43,7 +21,7 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		transform.Translate(camera.transform.forward * speed * Time.deltaTime);
+		transform.position = transform.position + camera.transform.forward * speed * Time.deltaTime;
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -55,4 +33,3 @@ public class PlayerController : MonoBehaviour {
 		}
 }
 }
->>>>>>> 503e0b73989c7b3eb1682a99d520acb9e0fc3100
