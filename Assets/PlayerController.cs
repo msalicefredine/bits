5417c31d03b4b721e56806478aa64ff43e2f05fa
Andrew Bates <andrewbatesubc@gmail.com>
2015-03-14 01:12:20 -0700

    many, many things

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,22 +6,30 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public Camera camera;
+	public GUIText text;
+	public int[] sequence;
+	public Rigidbody shipBody;
 	//public AudioSource goodSound;
 	//public AudioSource badSound;
 	// Use this for initialization
 	void Start () {
-
+		sequence = new int[10];
 		//AudioSource[] audios = GetComponents<AudioSource>();
 		//goodSound = audios [0];
 		//badSound = audios [1];
-	
-	
 	}
+
+	void OnCollisionEnter(){
+		rigidbody.angularVelocity = Vector3.zero;
+		rigidbody.velocity = Vector3.zero;
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
 
 		transform.position = transform.position + camera.transform.forward * speed * Time.deltaTime;
+
 	}
 	/*
 	void OnTriggerEnter(Collider other) {
