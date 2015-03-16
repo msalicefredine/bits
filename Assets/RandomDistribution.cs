300deed63f139e92483cf24f06cbc42682c49956
Alice Fredine <alice.fredine@gmail.com>
2015-03-15 21:13:02 -0700

    Added basic random distribution of notes to level 1

---
+++ Assets/RandomDistribution.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomObjects : MonoBehaviour {
+	
+	public GameObject parent;
+	
+	// Use this for initialization
+	void Start () {
+		
+		Transform[] allChildren = parent.GetComponentsInChildren<Transform> ();
+		
+		foreach (Transform child in allChildren)
+		{
+			
+			GameObject toDo = child.gameObject;
+			RandomTransform(toDo);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void RandomTransform(GameObject theObject) {
+		int x = Random.Range (1, 15);
+		int y = Random.Range (1, 15);
+		int z = Random.Range (1, 15);
+		
+		Vector3 trans = new Vector3 (x, y, z);
+		
+		theObject.transform.position = trans;
+		
+		
+	}
+}
