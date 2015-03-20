cd566faa970fba37eef4514a64576c138c07b08e
Alice Fredine <alice.fredine@gmail.com>
2015-03-19 18:59:39 -0700

    Bug fixes on transitions

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,47 +5,28 @@ using System.Reflection;
 
 public class Timer : MonoBehaviour {
 
-	//private float levelTime;
+	private float levelTime;
 	private float startTime;
 	public GUIStyle style;
 	// Use this for initialization
 	void Start () {
 		startTime = Time.deltaTime;
-		//levelTime = 0;
+		print ("starting timer");
 		//ALICE: Here is the variable you can work with: GameState.currentTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//levelTime = Time.deltaTime - startTime;
 		GameState.currentTime = Time.deltaTime - startTime;
-		/*levelTime -= Time.deltaTime;
-		if (levelTime < 20) {
-			style.normal.textColor = Color.yellow;
-		}
-		if (levelTime < 10) {
-			style.normal.textColor = Color.red;
-		}
-
-		if (levelTime <= 0) {
-			// end level, display menu to restart etc.
-		}*/
-		int minutes = (int) GameState.currentTime / 60; 
-		int seconds = (int) GameState.currentTime % 60; 
+		levelTime = Time.timeSinceLevelLoad;
+		// int minutes = (int) GameState.currentTime / 60; 
+		// int seconds = (int) GameState.currentTime % 60; 
+		int minutes = (int) levelTime / 60; 
+		int seconds = (int) levelTime % 60; 
 		
 		GameState.levelTime = String.Format ("{0:00}:{1:00}", minutes, seconds); 
-
+		print (GameState.levelTime);
+	}
 	
-	}
-
-	void OnGUI () {
-		int minutes = (int) GameState.currentTime / 60; 
-		int seconds = (int) GameState.currentTime % 60; 
-		// int fraction = (int) (levelTime * 100) % 100;
-
-		string timeText = String.Format ("{0:00}:{1:00}", minutes, seconds); 
-		// GUI.Label (new Rect (Screen.width*0.9f, Screen.height*0.9f, 100, 30), timeText, style); 
-
-	}
 }
